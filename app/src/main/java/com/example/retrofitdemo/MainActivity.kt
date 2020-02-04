c083d6c64545db839767bf0aa53e6d25e3f8d54d
salomirc <36732879+salomirc@users.noreply.github.com>
2020-02-04 11:50:57 +0200

    Retrofit2 get() suspend from RequestHelper.

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -1,31 +1,48 @@
 package com.example.retrofitdemo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.retrofitdemo.api.RequestHelper
+import com.example.retrofitdemo.models.Post
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private var toggle: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        progressBar.visibility = View.VISIBLE
-        CoroutineScope(Dispatchers.IO).launch {
-            val posts = RequestHelper.getPost()
-            withContext(Dispatchers.Main){
+        getDataButton.setOnClickListener {
+            resultTextView.text = ""
+            progressBar.visibility = View.VISIBLE
+            CoroutineScope(Dispatchers.Main).launch {
+
+                val posts = getPostsVM()
+
                 progressBar.visibility = View.INVISIBLE
-                for (item in posts!!) {
+                if (posts != null) {
+                    for (item in posts) {
                         resultTextView.append(item.toString())
                     }
-
+                }
+                else{
+                    resultTextView.text = "Network Error"
+                }
             }
+        }
+
+        toggleButton.setOnClickListener {
+            toggle = !toggle
+            if(toggle) (it as Button).setBackgroundColor(Color.MAGENTA) else (it as Button).setBackgroundColor(
+                Color.BLUE)
         }
 
 //        call.enqueue(object: Callback<List<Post>>{
@@ -50,4 +67,14 @@ class MainActivity : AppCompatActivity() {
 ////        })
 
     }
+
+    private suspend fun getPostsVM() : List<Post>? {
+        return RequestHelper.getPost()
+    }
+
+//    private suspend fun getPostsVM() : List<Post>? {
+//        return withContext(Dispatchers.IO){
+//            RequestHelper.getPost()
+//        }
+//    }
 }
--- app/src/main/java/com/example/retrofitdemo/api/RequestHelper.kt
+++ app/src/main/java/com/example/retrofitdemo/api/RequestHelper.kt
@@ -1,22 +1,32 @@
 package com.example.retrofitdemo.api
 
 import com.example.retrofitdemo.models.Post
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.awaitResponse
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 object RequestHelper {
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(2, TimeUnit.MINUTES)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .build()
+
     private val retrofit = Retrofit.Builder()
+        .client(okHttpClient)
         .baseUrl("https://jsonplaceholder.typicode.com/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     private val jsonPlaceHolderApi by lazy { retrofit.create(JsonPlaceHolderApi::class.java) }
 
-    fun getPost(): List<Post>?{
-
+    suspend fun getPost(): List<Post>?{
         try {
-            jsonPlaceHolderApi.getPost().execute().let { response ->
+            jsonPlaceHolderApi.getPost().awaitResponse().let { response ->
                 if (response.isSuccessful) return response.body()
             }
         }
@@ -25,4 +35,26 @@ object RequestHelper {
         }
         return null
     }
+
+//    suspend fun getPost(): List<Post>?{
+//        try {
+//            return jsonPlaceHolderApi.getPost()
+//        }
+//        catch (e: Exception){
+//            println("Exception : ${e.message}")
+//        }
+//        return null
+//    }
+
+//    fun getPost(): List<Post>?{
+//        try {
+//            jsonPlaceHolderApi.getPost().execute().let { response ->
+//                if (response.isSuccessful) return response.body()
+//            }
+//        }
+//        catch (e: Exception){
+//            println("Exception : ${e.message}")
+//        }
+//        return null
+//    }
 }
